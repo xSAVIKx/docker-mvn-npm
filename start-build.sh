0d1254d5498d1c4610b2f503d2c9ecede25a2026
Yurii Serhiichuk <savik.ne@gmail.com>
2023-10-15 10:56:22 +0200

    Migrate tag and version to specify Java version

--- start-build.sh
+++ start-build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-VERSION="mvn-3-node-14"
+VERSION="java8-mvn3-node14"
 GIT_SHA=$( git rev-parse HEAD )
 SHORT_SHA="${GIT_SHA::7}"
 IMAGE_NAME="xsavikx/docker-mvn-npm"
 BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-docker build \
+docker build -f "java8.Dockerfile" \
   --build-arg "BUILD_DATE=${BUILD_DATE}" \
   --build-arg "REVISION=${GIT_SHA}" \
   --build-arg "VERSION=${VERSION}" \
