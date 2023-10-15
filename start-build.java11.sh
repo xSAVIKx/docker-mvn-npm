29aa3e1ac600885bf08530e4f791f90ac236ff8f
Yurii Serhiichuk <savik.ne@gmail.com>
2023-10-15 11:25:04 +0200

    Migrate to Node 16

--- start-build.java11.sh
+++ start-build.java11.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export VERSION="java11-mvn3-node14"
+export VERSION="java11-mvn3-node16"
 export DOCKERFILE="java11.Dockerfile"
 ./start-build.sh
--- start-build.java8.sh
+++ start-build.java8.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export VERSION="java8-mvn3-node14"
+export VERSION="java8-mvn3-node16"
 export DOCKERFILE="java8.Dockerfile"
 ./start-build.sh
